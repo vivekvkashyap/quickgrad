a45e561bac3d6541b2ab60a13982236737e61abc
Lakshya <lakshyasinghaliitd@gmail.com>
2025-08-04 12:23:46 +0530

    new_version_1

--- quickgrad/core/engine.py
+++ quickgrad/core/engine.py
@@ -19,41 +19,28 @@ class vector:
     def __repr__(self):
         return f"vector(data={self.data})"
 
-    # Add __iadd__ to support += operations
-    # def __iadd__(self, other):
-    #     if isinstance(other, vector):
-    #         self.data += other.data
-    #     else:
-    #         self.data += other
-    #     return self
-
     def __add__(self, other):
         other = other if isinstance(other, vector) else vector(other)
         
-        # Handle broadcasting in forward pass
         out = vector(self.data + other.data, (self, other), '+')
         
         def _backward():
-            # Handle broadcasting in backward pass
+        
             self_grad = out.grad
             other_grad = out.grad
             
-            # If self was broadcast, sum gradients back to original shape
             if self.data.shape != out.grad.shape:
-                # Sum along broadcast dimensions and reshape
+            
                 self_grad = np.sum(out.grad, axis=None, keepdims=True)
                 while self_grad.ndim > len(self.data.shape):
                     self_grad = np.squeeze(self_grad, axis=0)
                 self_grad = np.broadcast_to(self_grad, self.data.shape)
-                
-                # Alternative: use proper axis reduction
+            
                 axes_to_sum = []
-                # Find axes that were broadcast
                 ndim_diff = len(out.grad.shape) - len(self.data.shape)
                 if ndim_diff > 0:
                     axes_to_sum.extend(range(ndim_diff))
                 
-                # Sum along broadcasted dimensions
                 for i in range(len(self.data.shape)):
                     if self.data.shape[i] == 1 and out.grad.shape[i + ndim_diff] > 1:
                         axes_to_sum.append(i + ndim_diff)
@@ -61,15 +48,13 @@ class vector:
                 if axes_to_sum:
                     self_grad = np.sum(out.grad, axis=tuple(axes_to_sum), keepdims=True)
                     self_grad = self_grad.reshape(self.data.shape)
-            
-            # If other was broadcast, sum gradients back to original shape  
+             
             if other.data.shape != out.grad.shape:
                 other_grad = np.sum(out.grad, axis=None, keepdims=True)
                 while other_grad.ndim > len(other.data.shape):
                     other_grad = np.squeeze(other_grad, axis=0)
                 other_grad = np.broadcast_to(other_grad, other.data.shape)
                 
-                # Alternative: use proper axis reduction
                 axes_to_sum = []
                 ndim_diff = len(out.grad.shape) - len(other.data.shape)
                 if ndim_diff > 0:
@@ -89,25 +74,6 @@ class vector:
         out._backward = _backward
         return out
     
-
-    # def __add__(self, other):
-    #     other = other if isinstance(other, vector) else vector(other)
-    #     # broadcast_shape = np.broadcast_shapes(self.shape(), other.shape())
-    #     # self, other = self.broadcast_to(broadcast_shape), other.broadcast_to(broadcast_shape)
-    #     out = vector(self.data + other.data, (self, other), '+')
-    
-    #     def _backward():
-    #         # if self.data.shape != out.grad.shape:
-    #             # self.grad = self.grad + np.sum(out.grad * np.ones_like(self.data))
-    #             # other.grad = other.grad + np.sum(out.grad * np.ones_like(other.data))
-    #         # else:
-    #         # self.grad = np.expand_dims(self.grad,0)
-    #         # other.grad = np.expand_dims(other.grad, 0)
-    #         self.grad +=  out.grad
-    #         other.grad +=  out.grad
-    #     out._backward = _backward
-    #     return out
-
     def exp(self):
         out = vector(np.exp(self.data), (self,), 'exp')
 
@@ -159,40 +125,13 @@ class vector:
             
         out._backward = _backward
         return out
-    
-    # def broadcast_to(self, target_shape):
-    #     input_shape = self.shape()
-    #     out = vector(np.broadcast_to(self.data, target_shape), (self,), 'broadcast')
 
-    #     def _backward():
-    #         print(input_shape)
-    #         print('bcf')
-    #         print(target_shape)
-    #         print('oo')
-    #         broadcast_axes_result = get_broadcast_axes(input_shape, target_shape)
-    #         broadcast_axes = broadcast_axes_result[0]
-    #         print('abc')
-    #         print(broadcast_axes_result)
-    #         # if broadcast_axes and isinstance(broadcast_axes[0], list):
-    #         #     broadcast_axes = broadcast_axes[0]
-    #         # ans = out.grad
-    #         # reduced_grad = ans
-            
-    #         # if broadcast_axes:  
-    #         #     axes_tuple = tuple(broadcast_axes)
-    #         #     reduced_grad = np.sum(ans, axis=axes_tuple, keepdims=True)
-    #         #     reduced_grad = reduced_grad.reshape(input_shape)
-    #         # self.grad += reduced_grad
-            
-    #     out._backward = _backward
-    #     return out
     
     def broadcast_to(self, shape):
 
         data = np.broadcast_to(self.data, shape)
         out = vector(data,  _children=(self, ), _op="broadcast")
-        broadcasted_axes = broadcast_axis(self.shape(), shape)[0] # we are interested in left axes
-
+        broadcasted_axes = broadcast_axis(self.shape(), shape)[0] 
         def _broadcast_backward():
             self.grad += np.sum(out.grad, axis=broadcasted_axes)
             
@@ -205,8 +144,6 @@ class vector:
             return vector(np.expand_dims(self.data, axis=axis))
         else:
             return vector(np.expand_dims(self, axis=axis))
-        # self = self if isinstance(self, vector) else vector(self)
-        # return vector(np.expand_dims(self, axis=axis))
     
     def transpose(self, axis):
         out = vector(np.transpose(self.data, axes=axis), (self,), 'T')
@@ -275,54 +212,6 @@ class vector:
         out._backward = _backward
         return out
 
-    # def __2dmatmul__(self, other):
-    #     out = vector(np.matmul(self.data, other.data), (self, other), '@')
-
-    #     def _backward():
-    #         other_data = other.transpose((1,0)).data
-    #         self_data = self.transpose((1,0)).data
-    #         self.grad += np.matmul(out.grad, other_data)
-    #         other.grad += np.matmul(self_data, out.grad)
-
-    #     out._backward = _backward
-    #     return out
-
-    # def __matmul__(self, other):
-        
-    #     # assert self._d == other._d, "Tensors must be of the same type i.e. numpy or mlx"
-    #     other = other if isinstance(other, vector) else vector(other, use_np=self.is_np_tensor)
-        
-    #     out = vector(self.data @ other.data, _children=(self, other), _op='@')
-    #     if not self.requires_grad and not other.requires_grad:
-    #         return out
-
-    #     le_axis = (0, ) if self.data.ndim == 1 else ()
-    #     re_axis = (-1, ) if other.data.ndim == 1 else ()
-    #     rese_axis = le_axis + re_axis
-    #     l, r = broadcast_axis(self.data.shape[:-2], other.data.shape[:-2])
-
-    #     def _matmul_backward():
-    #         if self.requires_grad:
-    #             self.grad = np.reshape(
-    #                 np.sum(
-    #                     np.expand_dims(out.grad, axis=rese_axis) @
-    #                     np.expand_dims(other.data, axis=re_axis).swapaxes(-1, -2),
-    #                     axis = l
-    #                 ),
-    #                 self.data.shape
-    #             )
-    #         if other.requires_grad:
-    #             other.grad = np.reshape(
-    #                 np.sum(
-    #                     np.expand_dims(self.data, axis=le_axis).swapaxes(-1, -2) @
-    #                     np.expand_dims(out.grad, axis=rese_axis),
-    #                     axis = r
-    #                 ),
-    #                 other.data.shape
-    #             )
-
-    #     out.grad_fn = _matmul_backward
-    #     return out
     
     def __matmul__(self, other):
         out = vector(np.matmul(self.data, other.data), (self, other), '@')
@@ -384,7 +273,6 @@ class vector:
         out = vector(clamped_data, (self,), 'clamp')
         
         def _backward():
-            # Gradient flows through where input is in valid range
             mask = (self.data >= min_val) & (self.data <= max_val)
             self.grad += mask * out.grad
         
@@ -402,21 +290,15 @@ class vector:
                 topo.append(v)
         build_topo(self)
         
-        # FIXED: Keep grad as numpy array, not vector
         self.grad = np.ones_like(self.data)
         for node in reversed(topo):
             node._backward()
 
-# Helper function (you'll need to implement this based on your needs)
 def get_broadcast_axes(input_shape, target_shape):
-    """
-    Returns the axes that were broadcasted from input_shape to target_shape
-    """
-    # Simple implementation - you may need to adjust based on your specific needs
+
     input_ndim = len(input_shape)
     target_ndim = len(target_shape)
     
-    # Pad input_shape with 1s at the beginning if needed
     padded_input = (1,) * (target_ndim - input_ndim) + input_shape
     
     broadcast_axes = []
@@ -424,21 +306,9 @@ def get_broadcast_axes(input_shape, target_shape):
         if inp_dim == 1 and tgt_dim > 1:
             broadcast_axes.append(i)
     
-    return [broadcast_axes], []  # Return in expected format
+    return [broadcast_axes], []  
 
 def broadcast_axis(left, right):
-    """
-    mlx uses broadcasting before performing array ops
-    this function determines which axes on either arrays will be broadcasted
-    in order to calculate gradients along those axes.
-
-    example:
-    >>> left.shape = (3, 1)
-    >>> right.shape = (1, 4)
-    >>> broadcast_axis(left, right)     # ((1, ), (0, ))
-
-    here the second axis for left, and first axis for right will be broadcasted
-    """
     
     ldim = len(left)
     rdim = len(right)
@@ -581,18 +451,6 @@ class Number:
 
     def __rtruediv__(self, other):  
         return (self ** -1) * other
-    
-    # def __lt__(self, other): 
-    #     return self.data < (other.data if isinstance(other, Number) else other)
-
-    # def __le__(self, other): 
-    #     return self.data <= (other.data if isinstance(other, Number) else other)
-    
-    # def __eq__(self, other): 
-    #     return self.data == (other.data if isinstance(other, Number) else other)
-    
-    # def __hash__(self):
-    #     return id(self)
     
     def backward(self):
         topo = []
